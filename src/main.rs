c08b2d314ddd69a082566236e8a71f3b220173dc
Pedro Silva <pedro@jibble.io>
2020-11-27 19:13:59 +0000

    First commit

---
+++ src/main.rs
@@ -0,0 +1,162 @@
+
+use lettre_email::Email;
+use lettre::smtp::authentication::Credentials;
+use lettre::{SmtpClient, Transport};
+use std::path::Path;
+use std::io::{Seek, Write};
+use chrono::prelude::*;
+use zip::write::ZipWriter;
+use zip::result::ZipError;
+use zip::write::FileOptions;
+
+use std::fs::File;
+use walkdir::{DirEntry, WalkDir};
+use std::io::prelude::*;
+use std::iter::Iterator;
+
+
+
+fn main() {
+    real_main();
+    send_email();
+}
+
+fn send_email() {
+    let now = Utc::now();
+    let timestamp_str = now.format("%Y-%m-%d %H:%M:%S").to_string();
+    let mut subject: String = "Jibble Test Report: ".to_owned();
+
+    subject.push_str(&timestamp_str);
+
+    let email = Email::builder()
+        .to("pedro@jibble.io")
+        .from("jibble.reporting.tool@gmail.com")
+        .subject(subject)
+        .html("<h1>Automated Tests Report</h1>")
+        .text("You received the report from tests ran at the time indicated on the subject. Check the attached files for more details")
+        .attachment_from_file(Path::new("./cypress/reports/report.zip"), None, &mime::APPLICATION_PDF)
+        .unwrap()
+        .build()
+        .unwrap();
+
+    let creds = Credentials::new(
+        "jibble.reporting.tool@gmail.com".to_string(),
+        "Speed Baby1.".to_string(),
+    );
+
+    // Open connection to gmail
+    let mut mailer = SmtpClient::new_simple("smtp.gmail.com")
+        .unwrap()
+        .credentials(creds)
+        .transport();
+
+    // Send the email
+    let result = mailer.send(email.into());
+
+    if result.is_ok() {
+        println!("Email sent");
+    } else {
+        println!("Could not send email: {:?}", result);
+    }
+
+    assert!(result.is_ok());
+}
+
+const METHOD_STORED: Option<zip::CompressionMethod> = Some(zip::CompressionMethod::Stored);
+
+#[cfg(any(
+    feature = "deflate",
+    feature = "deflate-miniz",
+    feature = "deflate-zlib"
+))]
+const METHOD_DEFLATED: Option<zip::CompressionMethod> = Some(zip::CompressionMethod::Deflated);
+#[cfg(not(any(
+    feature = "deflate",
+    feature = "deflate-miniz",
+    feature = "deflate-zlib"
+)))]
+const METHOD_DEFLATED: Option<zip::CompressionMethod> = None;
+
+#[cfg(feature = "bzip2")]
+const METHOD_BZIP2: Option<zip::CompressionMethod> = Some(zip::CompressionMethod::Bzip2);
+#[cfg(not(feature = "bzip2"))]
+const METHOD_BZIP2: Option<zip::CompressionMethod> = None;
+
+fn real_main() -> i32 {
+   
+    let src_dir = "./cypress/reports/mochawesome";
+    let dst_file =  "./cypress/reports/report.zip";
+    for &method in [METHOD_STORED, METHOD_DEFLATED, METHOD_BZIP2].iter() {
+        if method.is_none() {
+            continue;
+        }
+        match doit(src_dir, &dst_file, method.unwrap()) {
+            Ok(_) => println!("done: {} written to {}", src_dir, dst_file),
+            Err(e) => println!("Error: {:?}", e),
+        }
+    }
+
+    return 0;
+}
+
+fn zip_dir<T>(
+    it: &mut dyn Iterator<Item = DirEntry>,
+    prefix: &str,
+    writer: T,
+    method: zip::CompressionMethod,
+) -> zip::result::ZipResult<()>
+where
+    T: Write + Seek,
+{
+    let mut zip = zip::ZipWriter::new(writer);
+    let options = FileOptions::default()
+        .compression_method(method)
+        .unix_permissions(0o755);
+
+    let mut buffer = Vec::new();
+    for entry in it {
+        let path = entry.path();
+        let name = path.strip_prefix(Path::new(prefix)).unwrap();
+
+        // Write file or directory explicitly
+        // Some unzip tools unzip files with directory paths correctly, some do not!
+        if path.is_file() {
+            println!("adding file {:?} as {:?} ...", path, name);
+            #[allow(deprecated)]
+            zip.start_file_from_path(name, options)?;
+            let mut f = File::open(path)?;
+
+            f.read_to_end(&mut buffer)?;
+            zip.write_all(&*buffer)?;
+            buffer.clear();
+        } else if name.as_os_str().len() != 0 {
+            // Only if not root! Avoids path spec / warning
+            // and mapname conversion failed error on unzip
+            println!("adding dir {:?} as {:?} ...", path, name);
+            #[allow(deprecated)]
+            zip.add_directory_from_path(name, options)?;
+        }
+    }
+    zip.finish()?;
+    Result::Ok(())
+}
+
+fn doit(
+    src_dir: &str,
+    dst_file: &str,
+    method: zip::CompressionMethod,
+) -> zip::result::ZipResult<()> {
+    if !Path::new(src_dir).is_dir() {
+        return Err(ZipError::FileNotFound);
+    }
+
+    let path = Path::new(dst_file);
+    let file = File::create(&path).unwrap();
+
+    let walkdir = WalkDir::new(src_dir.to_string());
+    let it = walkdir.into_iter();
+
+    zip_dir(&mut it.filter_map(|e| e.ok()), src_dir, file, method)?;
+
+    Ok(())
+}
